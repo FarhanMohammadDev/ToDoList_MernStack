37bf1fc0581767d300cd11202a04ada91ae7afff
oumaimaezarouali <oumaima.2001ezarouali@gmail.com>
2024-01-29 15:43:15 +0100

    Connecting Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import process from "process"
+dotenv.config();
 const app = express();
-
 const port = 3000 ;
+
+const {MONGODB_URL} = process.env;
+
+mongoose.connect(MONGODB_URL)
+        .then(() => console.log("Database connected"))
+        .catch(err => console.log(err))
+
 app.use(express.json());
 
 app.get("/" , (req , res)=> {
